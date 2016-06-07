92fe41af5476a8e89fce5f7cd6e22bc1bd5d0366
root <root@vultr.guest>
2016-06-07 23:06:37 +0700

    InitAds

---
+++ partials/footads.js
@@ -0,0 +1,7 @@
+<ins class="adsbygoogle adsprofile"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-3851393091323474"
+     data-ad-slot="7671840544"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
---
+++ partials/sideads.js
@@ -0,0 +1,34 @@
+<style>
+@media(min-width: 1300px) {
+  .profile {
+    position: absolute;
+    right: 5%;
+    width: 15%;
+    top: 25px;
+  }
+}
+@media(max-width: 500px) { 
+  .adsprofile { width: 468px; height: 60px; } 
+}
+@media(max-width: 800px) { 
+     .adsprofile { 
+          width: 728px; height: 90px;
+     } 
+}
+.profile{
+  text-align: center;
+}
+</style>
+<div class="profile">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Pagi -->
+<ins class="adsbygoogle adsprofile"
+     style="display:block"
+          data-ad-client="ca-pub-3851393091323474"
+               data-ad-slot="7032712148"
+                    data-ad-format="auto"></ins>
+                    <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+
+</div>
