986703873e882f894ab26f98d37f381b84776034
SudoAsk <bruno.m4d@gmail.com>
2016-07-04 09:15:58 +0700

    Delete footads.js

--- partials/footads.js
+++
@@ -1,9 +0,0 @@
-<!-- Test -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3851393091323474"
-     data-ad-slot="3317222943"
-     data-ad-format="auto"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
